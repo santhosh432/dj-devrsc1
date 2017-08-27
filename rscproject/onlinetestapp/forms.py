db9de7e336a4233707428b093794009a79a9d4a6
santhosh <santhoshcusat17@gmail.com>
2017-08-27 16:56:01 +0530

    format changed

--- rscproject/onlinetestapp/forms.py
+++ rscproject/onlinetestapp/forms.py
@@ -4,16 +4,16 @@ import warnings
 
 
 class UserRegistrationForm(forms.Form):
-	username = forms.CharField(max_length=200, label=("User Name:"), required=True)
-	password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30)), label=("Password:"))
-	fname = forms.CharField(max_length=10, label=("First Name:"), required=True)
-	lname = forms.CharField(max_length=10, label=("Last Name:"), required=True)
-	emailid = forms.EmailField(max_length=100, label=("Email Address:"), required=True)
-	address=forms.CharField(max_length=200, widget=forms.Textarea,label=("Address:"))
-	phone =forms.IntegerField(label=("Phone"),widget=forms.TextInput)
-	tech = forms.CharField(max_length=20, label=("Select Technolgy:"), required=True)
+    username = forms.CharField(max_length=200, label=("User Name:"), required=True)
+    password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30)),
+                               label="Password:")
+    fname = forms.CharField(max_length=10, label=("First Name:"), required=True)
+    lname = forms.CharField(max_length=10, label=("Last Name:"), required=True)
+    emailid = forms.EmailField(max_length=100, label=("Email Address:"), required=True)
+    address = forms.CharField(max_length=200, widget=forms.Textarea, label=("Address:"))
+    phone = forms.IntegerField(label=("Phone"), widget=forms.TextInput)
+    tech = forms.CharField(max_length=20, label=("Select Technolgy:"), required=True)
 
 
 class ExfinalForm(forms.Form):
     original_ans = forms.CharField(max_length=2)
-
