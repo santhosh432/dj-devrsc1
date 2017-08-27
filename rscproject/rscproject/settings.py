6cf5da47cdda55dbd8ba763cdd26ae82e2af680b
santhosh <santhoshcusat17@gmail.com>
2017-08-27 16:55:06 +0530

    app created

--- rscproject/rscproject/settings.py
+++ rscproject/rscproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eh8@p!6b!p+vas@fb6s6y1km197#ki@lih+r*tpl#e8338-qc('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','127.0.0.1']
 
